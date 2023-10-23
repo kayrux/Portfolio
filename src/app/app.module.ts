cc5f646d6df0e39c15e5433eabc66cb5387492b7
Surya Kusjanto <skusjanto@outlook.com>
2023-10-22 23:35:19 -0600

    Use hash location strategy to prevent 404 when reloading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedAngularMaterialModule } from './shared/shared-modules/shared-angular-material.module';
@@ -26,7 +26,7 @@ import { VideoCardComponent } from './ui/carousel-card/video-card/video-card.com
     CarouselCardComponent,
     TextCardComponent,
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
